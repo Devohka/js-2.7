05746c0e6a95468550383b2ec27d664d2083a03c
Devohka <110058157+Devohka@users.noreply.github.com>
2024-01-13 12:37:26 +0200

    df

--- src/js/parcel.js
+++ src/js/parcel.js
@@ -5,12 +5,17 @@ const nameEl = document.querySelector("[data-name]");
 const btn = document.querySelector("[data-btn]");
 
 
+
+
 btn.addEventListener("click", (e) => {
     e.currentTarget
-    const user = {
-        name: `${nameEl}`,
-        text: `${text}`,
-        email: `${email}`
-    };
-    title.textContent = "Успішне відправлення";
+    
+        const user = {
+            name: `${nameEl}`,
+            text: `${text}`,
+            email: `${email}`
+        };
+        title.textContent = "Успішне відправлення";
 });
+
+
